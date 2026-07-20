a49d4ec5b245b64860f5b1a49e40077a5971081e
CoipoNorte <christiancaceres1398@gmail.com>
2026-07-20 18:54:21 -0400

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './Home.css'
 
 const MODES = [
